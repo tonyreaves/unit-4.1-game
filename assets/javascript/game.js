cd302b86964a78008cfdccc5593572dd5ada6f83
tonyreaves <tonyfreaves@gmail.com>
2018-05-20 09:57:03 -0700

    images, basic html

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+//listener event for document
+
+$(document).ready(function () {
+    new WOW().init();
+
+    // var audio = "assets/images/xxx.mp3";
+    // $().click(function () {
+    //   new Audio(audio).play();
+
+
+
+})
