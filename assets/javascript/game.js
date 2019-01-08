5b94c62d46ad9422966f89e3d4c77991bc7e731b
tonyreaves <tonyfreaves@gmail.com>
2019-01-08 12:15:13 -0700

    characters object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,45 @@
-//listener event for document
+$(document).ready(function() {
+  let characters = {
+    archangel: {
+      name: "archangel",
+      image: "assets/images/archangel.jpg",
+      health: 100,
+      attack: 10,
+      counter: 15
+    },
+    magneto: {
+      name: "magneto",
+      image: "assets/images/archangel.jpg",
+      health: 70,
+      attack: 15,
+      counter: 14
+    },
+    psylocke: {
+      name: "psylocke",
+      image: "assets/images/psylocke.jpg",
+      health: 90,
+      attack: 12,
+      counter: 14
+    },
+    storm: {
+      name: "storm",
+      image: "assets/images/storm.jpg",
+      health: 80,
+      attack: 16,
+      counter: 12
+    },
+    wolverine: {
+      name: "wolverine",
+      image: "assets/images/wolverine.jpg",
+      health: 80,
+      attack: 9,
+      counter: 11
+    }
+  };
 
-$(document).ready(function () {
-    new WOW().init();
+  let fighter = "";
+  let opponent = "";
+  let wins = 0;
 
-    // var audio = "assets/images/xxx.mp3";
-    // $().click(function () {
-    //   new Audio(audio).play();
-
-
-
-})
+  function attack() {}
+});
